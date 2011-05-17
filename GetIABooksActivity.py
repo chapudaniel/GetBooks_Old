400a0ccd5f4b5d7ccad4d4ac87862ee83feb9581
Daniel <chapu.daniel@gmail.com>
2011-05-17 20:56:59 +0200

    Primeros pasos catalogos

--- GetIABooksActivity.py
+++ GetIABooksActivity.py
@@ -488,6 +488,8 @@ class GetIABooksActivity(activity.Activity):
     def selection_cb(self, widget):
         # Testing...
         selected_book = self.listview.get_selected_book()
+        logging.debug("LIBROOOOOO")
+        logging.debug(selected_book._entry)
         if self.source == 'local_books':
             if selected_book:
                 self.download_url = ''
